1673d3a52c2d3ecf7f768cbfce9823e4d69af1f5
unknown <mmacknig@nd.edu>
2019-11-04 00:48:23 -0500

    post user

---
+++ backend/api/post_user.php
@@ -0,0 +1,42 @@
+<?php
+require 'database.php';
+
+// Get the posted data.
+$postdata = file_get_contents("php://input");
+
+if(isset($postdata) && !empty($postdata))
+{
+  // Extract the data.
+  $request = json_decode($postdata);
+
+
+  // Validate.
+  if(trim($request->username) === '' || trim($request->password) === '')
+  {
+    return http_response_code(400);
+  }
+
+  // Sanitize.
+  $username = mysqli_real_escape_string($con, trim($request->username));
+  $password = mysqli_real_escape_string($con, trim($request->password));
+
+
+  // Create.
+  $sql = "INSERT INTO `users`(`username`,`password`) VALUES ('{$username}','{$password}')";
+
+  if(mysqli_query($con,$sql))
+  {
+    http_response_code(201);
+    $user = [
+      'username' => $username,
+      'password' => $password,
+      'user_id'    => mysqli_insert_id($con)
+    ];
+    echo json_encode($user);
+  }
+  else
+  {
+    http_response_code(422);
+  }
+}
+?>
