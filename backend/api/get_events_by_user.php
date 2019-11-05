41f9b433b11fce006f7936a12609085292aa4d03
Dominic Bozzo <dbozzo@nd.edu>
2019-11-05 17:46:59 -0500

    get events by user id added

---
+++ backend/api/get_events_by_user.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Returns the list of policies.
+ */
+require 'database.php';
+
+// Extract, validate and sanitize the id.
+$user_id = ($_GET['id'] !== null && (int)$_GET['id'] > 0)? mysqli_real_escape_string($con, (int)$_GET['id']) : false;
+
+$events = [];
+$sql = "SELECT event_id, user_id, event_name, description, current_song, is_active FROM events where events.user_id = {$user_id} and is_active=1 limit 1";
+
+if($result = mysqli_query($con,$sql))
+{
+  $i = 0;
+  while($row = mysqli_fetch_assoc($result))
+  {
+    $events[$i]['event_id'] = $row['event_id'];
+    $events[$i]['user_id'] = $row['user_id'];
+    $events[$i]['event_name'] = $row['event_name'];
+    $events[$i]['description'] = $row['description'];
+    $events[$i]['current_song'] = $row['current_song'];
+    $events[$i]['is_active'] = $row['is_active'];
+    $i++;
+  }
+  
+  echo json_encode($events);
+  
+  http_response_code(200);
+  
+  
+}
+else
+{
+  // no username found
+  http_response_code(404);
+}
+?>
