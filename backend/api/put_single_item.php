3bec8d3b536dafdfc4a95f54c27a234ddb129561
James Philhower <jphilhow@nd.edu>
2019-11-12 16:23:24 -0500

    updated names

--- backend/api/put_single_item.php
+++ backend/api/put_single_item.php
@@ -1,44 +1,54 @@
 <?php
 require 'database.php';
+//require "put_post_helper.php";
+require "put_helper.php";
 
-// Get the posted data.
+class Data {
+    public $isValid = false;
+    public $content;
+}
+
+
+$table = mysqli_real_escape_string($con, $_GET['table']);
+
 $postdata = file_get_contents("php://input");
 
-if(isset($postdata) && !empty($postdata))
-{
-  // Extract the data.
-  $class_object = json_decode($postdata);
-  
+$data = new Data();
+$data->isValid = true;
 
-  $things_to_update = [];
-  
-  $table = null;
+if(isset($postdata) && !empty($postdata) && $data->isValid){
 
-  $id = null;
+    $columns_to_update = [];
+    $request_content = json_decode($postdata);
 
-  foreach($class_object as $key => $value) {
-      $secure_value = mysqli_real_escape_string($con, $value);
-      $temp_string = "`".$key."`=" . $secure_value;
-      array_push($things_to_update, $temp_string);
-      if (strpos($key, "id")){
-        $table = $key."s";
-        $id = $value;
-      }
-  }
-  
-  $update_string = implode(",", $things_to_update);
 
-  // Update.
-  $sql = "UPDATE '{$table}' SET '{$update_string}' WHERE '{$table}' = '{$id}' LIMIT 1";
+    foreach($request_content as $key => $value) {
+        $secure_key = mysqli_real_escape_string($con, $key);
+        $secure_value = mysqli_real_escape_string($con, $value);
 
-  if(mysqli_query($con, $sql))
-  {
-    http_response_code(204);
-  }
-  else
-  {
-    return http_response_code(422);
-  }
+        $secure_value = check_based_on_self($secure_value, $secure_key);
+
+        $temp_string = $secure_key."=".$secure_value;
+        array_push($columns_to_update, $temp_string);
+
+        if ($secure_key == $table."_id"){
+          $id = (int)$secure_value;
+        }
+    }
+
+    $update_string = implode(",", $columns_to_update);
+
+    // Update.
+    $sql = "UPDATE `{$table}s` SET {$update_string} WHERE {$table}_id = {$id} LIMIT 1";
+
+    if(mysqli_query($con, $sql))
+    {
+      http_response_code(204);
+    }
+    else
+    {
+      return http_response_code(422);
+    }
 
 }
 ?>
