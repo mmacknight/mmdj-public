863628dc6a3f6b7d0f38c171588cb329fa017659
James Philhower <jphilhow@nd.edu>
2019-11-12 17:24:59 -0500

    votes

--- backend/api/get_votes_single_user.php
+++ backend/api/get_votes_single_user.php
@@ -5,12 +5,18 @@ require 'database.php';
 $user_id = ($_GET['user_id'] !== null )? mysqli_real_escape_string($con, trim($_GET['user_id'])) : false;
 $event_id = ($_GET['event_id'] !== null )? mysqli_real_escape_string($con, trim($_GET['event_id'])) : false;
 
-$sql = "SELECT vote FROM votes WHERE user_id = '{$user_id}' and event_id = '{$event_id}'";
-
+$sql = "SELECT * FROM votes WHERE user_id = '{$user_id}' and event_id = '{$event_id}'";
+$songs = [];
 if($result = mysqli_query($con,$sql))
 {
-    $row = mysqli_fetch_assoc($result);
-    echo json_encode($row);
+$i = 0;
+  while($row = mysqli_fetch_assoc($result))
+  {
+    $songs[$i] =  $row;
+    $i++;
+  }
+
+  echo json_encode($songs);
 }
 else
 {
