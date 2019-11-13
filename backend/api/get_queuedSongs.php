1788f593faef41ccb95b52df3a269da20930e370
Dominic Bozzo <dbozzo@nd.edu>
2019-11-13 15:30:04 -0500

    updating get_queuedSongs

--- backend/api/get_queuedSongs.php
+++ backend/api/get_queuedSongs.php
@@ -8,7 +8,7 @@ require 'database.php';
 $event_id = ($_GET['id'] !== null && (int)$_GET['id'] > 0)? mysqli_real_escape_string($con, (int)$_GET['id']) : false;
 
 $songs = [];
-$sql = "SELECT song_id, title, artist, genre, duration_ms, order_num, queuedSongs.popularity, playability FROM songs, queuedSongs where queuedSongs.event_id = {$event_id} and songs.song_id = queuedSongs.queuedSongs_id and playability = true ORDER BY queuedSongs.popularity desc, queuedSongs.order_num";
+$sql = "SELECT songs.* , queuedSongs.popularity as qpop, queuedSongs.playability FROM songs, queuedSongs where queuedSongs.event_id = {$event_id} and songs.song_id = queuedSongs.queuedSongs_id and playability = true ORDER BY queuedSongs.popularity desc, queuedSongs.order_num";
 
 if($result = mysqli_query($con,$sql))
 {
@@ -21,10 +21,24 @@ if($result = mysqli_query($con,$sql))
     $songs[$i]['genre'] = $row['genre'];
     $songs[$i]['duration_ms'] = $row['duration_ms'];
     $songs[$i]['order_num'] = $row['order_num'];
-    $songs[$i]['popularity'] = $row['popularity'];
+    $songs[$i]['popularity'] = $row['qpop'];
     $songs[$i]['playability'] = $row['playability'];
+    $songs[$i]['track_id'] = $row['track_id'];
+    $songs[$i]['danceability'] = $row['danceability'];
+    $songs[$i]['energy'] = $row['energy'];
+    $songs[$i]['instrumental'] = $row['instrumental'];
+    $songs[$i]['music_key'] = $row['music_key'];
+    $songs[$i]['liveness'] = $row['loudness'];
+    $songs[$i]['loudness'] = $row['loudness'];
+    $songs[$i]['mode'] = $row['mode'];
+    $songs[$i]['speechiness'] = $row['speechiness'];
+    $songs[$i]['tempo'] = $row['tempo'];
+    $songs[$i]['time_signature'] = $row['time_signature'];
+    $songs[$i]['valence'] = $row['valence'];
+    $songs[$i]['order_num'] = $row['order_num'];
     $i++;
   }
+
   
   echo json_encode($songs);
   
