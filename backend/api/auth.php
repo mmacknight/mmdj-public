35f4b2b8f5ef0aa6590341dead50931849b08df3
Dominic Bozzo <dbozzo@nd.edu>
2019-12-12 15:34:51 -0500

    all auth scopes added'

--- backend/api/auth.php
+++ backend/api/auth.php
@@ -19,9 +19,25 @@ $session = new SpotifyWebAPI\Session(
 
 $options = [
     'scope' => [
-        'playlist-read-private',
+        'ugc-image-upload',
+        'user-read-playback-state',
+        'user-modify-playback-state',
+        'user-read-currently-playing',
+        'streaming',
+        'app-remote-control',
+        'user-read-email',
         'user-read-private',
-        'streaming'
+        'playlist-read-collaborative',
+        'playlist-modify-public',
+        'playlist-read-private',
+        'playlist-modify-private',
+        'user-library-modify',
+        'user-library-read',
+        'Listening History',
+        'user-top-read',
+        'user-read-recently-played',
+        'user-follow-read',
+        'user-follow-modify'
     ],
 ];
 
